458bb73b9bc5a9b2229999b1ac5d7725a8646923
Roshan Singh bhadauriya <roshanbhadoriya178@gmail.com>
2024-10-29 20:13:21 +0530

    done with edit functionality

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ function addTodo() {
   render();
 }
 
-
-
-
 function createComponent(todo, i) {
   let newDiv = document.createElement("div");
   let p = document.createElement("p");
@@ -21,10 +18,10 @@ function createComponent(todo, i) {
 
   let edit = document.createElement("a");
   edit.className = "edit";
-  edit.appendChild(document.createTextNode("edit"));
+  edit.appendChild(document.createTextNode("Edit"));
   let deleteit = document.createElement("a");
   deleteit.className = "delete";
-  deleteit.appendChild(document.createTextNode("delete"));
+  deleteit.appendChild(document.createTextNode("Delete"));
 
   newDiv.appendChild(p);
   newDiv.appendChild(edit);
@@ -51,15 +48,24 @@ form.addEventListener("submit", function (e) {
   document.querySelector("#todos").value = "";
 });
 
+document.querySelector(".bounty-area").addEventListener("click", function (e) {
+  if (e.target.className === "edit") {
+    let parent = e.target.parentNode;
+    let ids = parent.id;
+    const newvalue = todos[ids].title;
+    document.querySelector("#todos").value = newvalue;
+    const input = document.querySelector("#todos");
+    input.focus();
+    todos.splice(ids, 1);
+    render();
+  }
+});
 
-
-document.querySelector(".bounty-area").addEventListener("click" , function(e){
- 
-    if(e.target.className === "delete"){
-       let parent = e.target.parentNode;
-       let ids = parent.id;
-       todos.splice(ids,1);
-       render();
-    }
-     
-   })
+document.querySelector(".bounty-area").addEventListener("click", function (e) {
+  if (e.target.className === "delete") {
+    let parent = e.target.parentNode;
+    let ids = parent.id;
+    todos.splice(ids, 1);
+    render();
+  }
+});
